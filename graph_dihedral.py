98b67bf707bdfdf1517541fd9a89ecbc3105ea92
Mohammad Madani <66979328+mahan-fcb@users.noreply.github.com>
2024-04-11 16:52:00 -0400

    Update graph_dihedral.py

--- graph_dihedral.py
+++ graph_dihedral.py
@@ -1,221 +1,328 @@
-#this code has been obtained from ALIGNN-D
-import torch
-from torch_geometric.data import Data
-import torch
-from torch.linalg import cross
-
-
-def bond_angles(bond_vec, edge_index_bnd_ang):
-    bond_vec /= torch.linalg.norm(bond_vec, dim=-1, keepdim=True)
-    i = edge_index_bnd_ang[0]
-    j = edge_index_bnd_ang[1]
-    cos_ang = (bond_vec[i] * bond_vec[j]).sum(dim=-1, keepdim=True).clamp(min=-1., max=1.)
-    sin_ang = cos_ang.acos().sin()
-    return torch.hstack([cos_ang, sin_ang])
-
-
-def dihedral_angles(pos, edge_index_bnd, edge_index_dih_ang):
-    """Does not account for periodic boundaries"""
-    dih_idx = edge_index_bnd.T[edge_index_dih_ang.T].reshape(-1, 4)
-    dih_idx = dih_idx.T
-    i, j, k, l = dih_idx[0], dih_idx[1], dih_idx[3], dih_idx[2]
-    u1 = pos[j] - pos[i]
-    u2 = pos[k] - pos[j]
-    u3 = pos[l] - pos[k]
-    u1 /= torch.linalg.norm(u1, dim=-1, keepdim=True)
-    u2 /= torch.linalg.norm(u2, dim=-1, keepdim=True)
-    u3 /= torch.linalg.norm(u3, dim=-1, keepdim=True)
-    cos_ang = (cross(u1, u2) * cross(u2, u3)).sum(dim=-1, keepdim=True).clamp(min=-1., max=1.)
-    sin_ang = (u1 * cross(u2, u3)).sum(dim=-1, keepdim=True).clamp(min=-1., max=1.)
-    return torch.hstack([cos_ang, sin_ang])
-
-
-class MolData(Data):
-    def __inc__(self, key, value, *args, **kwargs):
-        if key in {'edge_index', 'bnd_index', 'aux_bnd_index'}:
-            return self.x_atm.size(0)
-        if key in {'ang_index', 'bnd_ang_index', 'dih_ang_index'}:
-            return self.x_bnd.size(0)
-        return super().__inc__(key, value, *args, **kwargs)
-
-    def bond_features(self, bond_index_name='bnd_index'):
-        """Does not account for periodic boundaries"""
-        i, j = self.__getattr__(bond_index_name)
-        bond_vec = self.pos[j] - self.pos[i]
-        bond_len = bond_vec.norm(dim=-1, keepdim=True)
-        return torch.hstack([bond_vec, bond_len])
-
-    def bond_angle_features(self, bond_feature_name='x_bnd', bond_angle_index_name='bnd_ang_index'):
-        x_bnd = self.__getattr__(bond_feature_name)
-        bnd_ang_index = self.__getattr__(bond_angle_index_name)
-        return bond_angles(x_bnd[:, :3], bnd_ang_index)
-    
-    def dihedral_angle_features(self, bond_index_name='bnd_index', dihehdral_angle_index_name='dih_ang_index'):
-        """Does not account for periodict boundaries"""
-        bnd_index = self.__getattr__(bond_index_name)
-        dih_ang_index = self.__getattr__(dihehdral_angle_index_name)
-        return dihedral_angles(self.pos, bnd_index, dih_ang_index)
-    
-    def concat_features_with_onehot(self, x1_name='x_bnd', x2_name='x_aux_bnd'):
-        x1 = self.__getattr__(x1_name)
-        x2 = self.__getattr__(x2_name)
-        mask = torch.cat([
-            torch.zeros(x1.size(0), dtype=torch.long, device=self.pos.device),
-            torch.ones(x2.size(0),  dtype=torch.long, device=self.pos.device),
-        ])
-        onehot = torch.nn.functional.one_hot(mask)
-        return torch.hstack([torch.vstack([x1, x2]), onehot])
-    
-    def get_bnd_ang_vals(self):
-        cos_ang, _ = self.x_bnd_ang[:, :2].T
-        return cos_ang.arccos().rad2deg()
-    
-    def get_dih_ang_vals(self):
-        cos_ang, sin_ang = self.x_dih_ang[:, :2].T
-        return torch.atan2(sin_ang, cos_ang).rad2deg()
-
-
-# In[7]:
-
-
-import torch
+from time import time
+from pymatgen.core.periodic_table import Element
 import numpy as np
+import torch
+from itertools import combinations, product
+class Graph(object):
+    '''
+    Graph object for creation of atomic graphs with bond and node attributes from pymatgen structure
+    '''
 
-from ase.neighborlist import primitive_neighbor_list
+    def __init__(
+        self,
+        neighbors=12,
+        rcut=8,
+        delta=1,
+    ):
+
+        self.neighbors = neighbors
+        self.rcut = rcut
+        self.delta = delta
+        self.bond = []
+        self.nbr = []
+        self.angle_cosines = []
+        self.angle_sines = []
+        self.dihedral_angles = []
+
+    def setGraphFea(self, structure):
+
+        if self.rcut > 0:
+            pass
+        else:
+            species = [site.specie.symbol for site in structure.sites]
+            self.rcut = max(
+                [Element(elm).atomic_radius * 3 for elm in species]
+            )
+
+        all_nbrs = structure.get_all_neighbors(self.rcut, include_index=True)
+
+        len_nbrs = np.array([len(nbr) for nbr in all_nbrs])
+
+        indexes = np.where((len_nbrs < self.neighbors))[0]
+
+        for i in indexes:
+            cut = self.rcut
+            curr_N = len(all_nbrs[i])
+            while curr_N < self.neighbors:
+                cut += self.delta
+                nbr = structure.get_neighbors(structure[i], cut)
+                curr_N = len(nbr)
+            all_nbrs[i] = nbr
+
+        all_nbrs = [sorted(nbrs, key=lambda x: x[1]) for nbrs in all_nbrs]
+
+        self.nbr = torch.LongTensor(
+            [
+                list(map(lambda x: x[2], nbrs[: self.neighbors]))
+                for nbrs in all_nbrs
+            ]
+        )
+        self.bond = torch.Tensor(
+            [
+                list(map(lambda x: x[1], nbrs[: self.neighbors]))
+                for nbrs in all_nbrs
+            ]
+        )
+
+        cart_coords = torch.Tensor(np.array(
+            [structure[i].coords for i in range(len(structure))]
+        ))
+        atom_nbr_fea = torch.Tensor(np.array(
+            [
+                list(map(lambda x: x[0].coords, nbrs[: self.neighbors]))
+                for nbrs in all_nbrs
+            ]
+        ))
+        centre_coords = cart_coords.unsqueeze(1).expand(
+            len(structure), self.neighbors, 3
+        )
+        dxyz = atom_nbr_fea - centre_coords
+        r = self.bond.unsqueeze(2)
+        self.angle_cosines = torch.matmul(
+            dxyz, torch.swapaxes(dxyz, 1, 2)
+        ) / torch.matmul(r, torch.swapaxes(r, 1, 2))
+
+        # Calculate sine of bond angles
+       # dxyz_reshaped = dxyz.unsqueeze(2).repeat(1, 1, self.neighbors, 1)
+
+# Compute cross product
+       # cross_product = torch.cross(dxyz_reshaped, torch.swapaxes(dxyz, 1, 2))
+       # sine_denominator = torch.norm(cross_product, dim=2)
+       # self.angle_sines = torch.clamp(sine_denominator / (r ** 2), min=-1.0, max=1.0)
+        self.calculate_dihedral_angles(structure)
+
+    def calculate_dihedral_angles(self, structure):
+        """
+        Calculates dihedral angles for each set of four atoms and structures them into a consistent shape.
+        """
+        max_dihedrals_per_atom = self.neighbors * (self.neighbors - 1) // 2
+        dihedral_angles = torch.zeros((len(structure), max_dihedrals_per_atom), dtype=torch.float)
+
+        for i in range(len(structure)):
+            angles = []
+            nbr_indices = self.nbr[i].tolist()
+            for j, k, l in combinations(nbr_indices, 3):
+                if j != i and k != i and l != i and j != k and j != l and k != l:
+                    p0, p1, p2, p3 = [structure[idx].coords for idx in [i, j, k, l]]
+                    angle = self.calculate_single_dihedral_angle(torch.tensor(p0), torch.tensor(p1), torch.tensor(p2), torch.tensor(p3))
+                    if not np.isnan(angle):  # Check if angle calculation is valid
+                        angles.append(angle)
+
+            # Fill the tensor for the current atom with calculated angles up to max_dihedrals_per_atom
+            dihedral_angles[i, :len(angles)] = torch.tensor(angles, dtype=torch.float)[:max_dihedrals_per_atom]
+
+        self.dihedral_angles = dihedral_angles
+
+    def calculate_single_dihedral_angle(self, p0, p1, p2, p3):
+        """
+        Safely calculate a single dihedral angle given four points.
+        """
+        b0 = p1 - p0
+        b1 = p2 - p1
+        b2 = p3 - p2
+
+        n1 = torch.cross(b0, b1)
+        n2 = torch.cross(b1, b2)
+
+        # Avoid division by zero by adding a small epsilon
+        epsilon = 1e-10
+        n1_u = n1 / (torch.norm(n1) + epsilon)
+        n2_u = n2 / (torch.norm(n2) + epsilon)
+
+        # Calculate the angle using atan2 for a full range angle
+        x = torch.dot(n1_u, n2_u)
+        m1 = torch.cross(n1_u, b1 / (torch.norm(b1) + epsilon))
+        y = torch.dot(m1, n2_u)
+
+        angle = torch.atan2(y, x)
+
+        return angle.item()
+
+class load_graphs_targets(object):
+
+    '''
+    structureData should
+    be in dict format
+                      structure:{pymatgen structure},
+                      property:{}
+                      formula: None or formula
+    if not from database
+    '''
+
+    def __init__(self, neighbors=12, rcut=0, delta=1):
+
+        self.neighbors = neighbors
+        self.rcut = rcut
+        self.delta = delta
+
+    def load(self, data):
+        structure = data["structure"]
+        target = data["target"]
+        # print(target)
+        graph = Graph(
+            neighbors=self.neighbors, rcut=self.rcut, delta=self.delta
+        )
+        # try:
+        # print("graphs")
+        graph.setGraphFea(structure)
+        print(data["formula"])
+        return (graph, target)
+        # except:
+        #    return None
+        
+class CrystalGraphDataset(Dataset):
+    '''
+    A Crystal graph dataset container for genrating and loading pytorch dataset to be passed to train test and validation loader
+    '''
+
+    def __init__(
+        self,
+        dataset,
+        neighbors=12,
+        rcut=8,
+        delta=1,
+        mp_load=False,
+        mp_pool=None,
+        mp_cpu_count=None,
+        **kwargs
+    ):
+
+        # ================================
+        print("Loading {} graphs .......".format(len(dataset)))
+        # =================================
+
+        t1 = time()
+
+        load_graphs = load_graphs_targets(
+            neighbors=neighbors,
+            rcut=rcut,
+            delta=delta,
+        )
+
+        results = process(
+            load_graphs.load,
+            dataset,
+            mp_cpu_count,
+            mp_load=mp_load,
+            mp_pool=mp_pool,
+        )
+
+        # print(results)
+
+        self.graphs = [res[0] for res in results if res is not None]
+
+       # self.targets = [
+       #     torch.LongTensor(res[1]) for res in results if res is not None
+        #]
+        # print(self.targets)
+        #self.binarizer = LabelBinarizer()
+        #self.binarizer.fit(torch.cat(self.targets))
+
+        t2 = time()
+        print("Total time taken {}".format(convert(t2 - t1)))
+
+        #self.size = len(self.targets)
+
+    def collate(self, datalist):
+
+        bond_feature, nbr_idx, angular_feature, dihedral, crys_idx = (
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+    
+    
+        )
+
+        index = 0
+
+        for (bond_fea, idx, angular_fea,ang,dih), targ in datalist:
+            Natoms = bond_fea.shape[0]
+
+            bond_feature.append(bond_fea)
+            angular_feature.append(angular_fea)
+            dihedral.append(dih)
+
+            nbr_idx.append(idx + index)
+            crys_idx.append([index, index + Natoms])
+           # targets.append(targ)
+            index += Natoms
+
+        return (
+            torch.cat(bond_feature, dim=0),
+            torch.cat(angular_feature, dim=0),
+            torch.cat(dihedral, dim=0),
+            torch.cat(nbr_idx, dim=0),
+            torch.LongTensor(crys_idx),
+            
+        )
+
+    def __getitem__(self, idx):
+
+        graph = self.graphs[idx]
+        bond_feature = graph.bond
+        nbr_idx = graph.nbr
+        angular_feature = graph.angle_cosines
+        dihedral = graph.dihedral_angles
+    
+       # target = self.targets[idx]
+
+        return (bond_feature, nbr_idx, angular_feature,dihedral)
 
 
-def ase_radius_graph(pos, cutoff, numbers=None, cell=np.diag([1.,1.,1.]), pbc=[False]*3):
-    """Computes graph edges based on a cutoff radius for 3D structure data with periodic boundaries.
-    Returns the edge indices `edge_index` and the edge vectors `edge_vec`.
+# --------------------------------------------
+def process(func, tasks, n_proc, mp_load=False, mp_pool=None):
 
-    This implementation uses ASE's neighbor list algorithm, which accounts for periodic boundaries.
-    """
-    pos_  = pos.clone().detach().cpu().numpy()
-    i, j, S = primitive_neighbor_list('ijS', positions=pos_, cell=cell, cutoff=cutoff, pbc=pbc, numbers=numbers)
-    i = torch.tensor(i, dtype=torch.long,  device=pos.device)
-    j = torch.tensor(j, dtype=torch.long,  device=pos.device)
-    S = torch.tensor(S, dtype=torch.float, device=pos.device)
-    edge_index = torch.stack([i, j])
-    edge_vec = pos[j] - pos[i] + S@cell
-    return edge_index, edge_vec
+    if mp_load:
+        results = []
+        chunks = [tasks[i : i + n_proc] for i in range(0, len(tasks), n_proc)]
+        for chunk in chunks:
+            # print("chunks")
+            r = mp_pool.map_async(func, chunk, callback=results.append)
+            r.wait()
+        mp_pool.close()
+        mp_pool.join()
+        return results[0]
+    else:
+        # print("chunks")
+        return [func(task) for task in tasks]
 
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
 
-# In[8]:
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
 
+import csv
+from pymatgen.io.cif import CifParser
+import os
 
-import numpy as np
-import pandas as pd
+# Define your dataset loader function
+def load_dataset(directory, target_property_file, neighbors=12, rcut=3, delta=1):
+    loader = load_graphs_targets(neighbors=neighbors, rcut=rcut, delta=delta)
+    dataset = []
+    target_file_path = os.path.join(directory, target_property_file)
 
+    with open(target_file_path) as f:
+        reader = csv.reader(f)
+        target_data = [row for row in reader][1:]  # Skip header
 
-mask2index = lambda mask: np.flatnonzero(mask)
-
-
-def index2mask(idx_arr, n):
-    mask = np.zeros(n, dtype=int)
-    mask[idx_arr] = 1
-    return mask.astype(np.bool)
-
-
-def np_groupby(arr, groups):
-    """Numpy implementation of `groupby` operation (a common method in pandas).
-    """
-    arr, groups = np.array(arr), np.array(groups)
-    sort_idx = groups.argsort()
-    arr = arr[sort_idx]
-    groups = groups[sort_idx]
-    return np.split(arr, np.unique(groups, return_index=True)[1])[1:]
-
-
-def np_scatter(src, index, func):
-    """Generalization of the `torch_scatter.scatter` operation for any reduce function.
-    See https://pytorch-scatter.readthedocs.io/en/latest/functions/scatter.html for how `scatter` works.
-
-    Args:
-        src (array): The source array.
-        index (array of int): The indices of elements to scatter.
-        func (function): Reduce function (e.g., mean, sum) that operates on elements with the same indices.
-
-    :rtype: generator
-    """
-    return (func(g) for g in np_groupby(src, index))
-
-
-# In[9]:
-
-
-import numpy as np
-import itertools
-from functools import partial
-
-
-permute_2 = partial(itertools.permutations, r=2)
-def line_graph(edge_index_G):
-    """Return the (angular) line graph of the input graph.
-
-    Args:
-        edge_index_G (ndarray): Input graph in COO format.
-    """
-    src_G, dst_G = edge_index_G
-    edge_index_A = [
-        (u, v)
-        for edge_pairs in np_scatter(np.arange(len(dst_G)), dst_G, permute_2)
-        for u, v in edge_pairs
-    ]
-    return np.array(edge_index_A).T
-
-
-def dihedral_graph(edge_index_G):
-    """Return the "dihedral angle line graph" of the input graph.
-
-    Args:
-        edge_index_G (ndarray): Input graph in COO format.
-    """
-    src, dst = edge_index_G
-    edge_index_A = [
-        (u, v)
-        for i, j in edge_index_G.T
-        for u in np.flatnonzero((dst == i) & (src != j))
-        for v in np.flatnonzero((dst == j) & (src != i))
-    ]
-    return np.array(edge_index_A).T
-
-def atoms2molgraph(atoms, cutoff):
-    """Returns edge indices of atomic bonds according to a cutoff criteria.  
-    """
-    i, j = neighbor_list('ij', atoms, cutoff=3)
-    return np.stack((i, j))
-
-def atoms2pygdata(atoms):
-    """Converts ASE `atoms` into a PyG graph data holding the molecular graph (G) and the angular graph (A).
-    The angular graph holds both bond and dihedral angles.
-    """
-    encoder = OneHotEncoder()
-
-    # Reshape the atomic numbers array for encoding
-    numbers_reshaped = atoms.numbers.reshape(-1, 1)
-
-    # Fit and transform the atomic numbers
-    x_atm = encoder.fit_transform(numbers_reshaped).toarray() 
-    edge_index_bnd = atoms2molgraph(atoms, cutoff=3)
-    edge_index_bnd_ang = line_graph(edge_index_bnd)
-    edge_index_dih_ang = dihedral_graph(edge_index_bnd)
-
-    data = MolData(
-        pos                = torch.tensor(atoms.positions,    dtype=torch.float),
-        x_atm              = torch.tensor(x_atm,              dtype=torch.float),
-        edge_index_bnd     = torch.tensor(edge_index_bnd,     dtype=torch.long),
-        edge_index_bnd_ang = torch.tensor(edge_index_bnd_ang, dtype=torch.long),
-        edge_index_dih_ang = torch.tensor(edge_index_dih_ang, dtype=torch.long),
-    )
-    return data
-
-@torch.no_grad()
-def update_bonds_and_angles(data, batch=None):
-
-    data.x_bnd = data.bond_features('edge_index_bnd')
-    data.x_bnd_ang = data.bond_angle_features('x_bnd', 'edge_index_bnd_ang')
-    data.x_dih_ang = data.dihedral_angle_features('edge_index_bnd', 'edge_index_dih_ang')
-    data.x_ang = data.concat_features_with_onehot('x_bnd_ang', 'x_dih_ang')
-    data.edge_index_ang = torch.hstack([data.edge_index_bnd_ang, data.edge_index_dih_ang])
-    return data
-
+    for row in target_data:
+        structure_id = row[0]
+        cif_file_path = os.path.join(directory, structure_id + ".cif")
+        # Load structure data from the CIF file
+        parser = CifParser(cif_file_path)
+        structure = parser.get_structures()[0]  # Assuming there's only one structure in the CIF file
+        # Here, assuming the target property is in the second column of the CSV file
+        target = float(row[1])  # Convert the target value from string to float
+        # Load graph features and target
+        a = {"structure": structure, "target": target, "formula": structure_id + ".cif"}
+        
+        dataset.append(a)
+    
+    return dataset
